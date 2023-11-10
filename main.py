7b6138b16af49a397e1c5ec0d8671738cd0d490b
Viktor <va.kuroedov@gmail.com>
2023-11-11 00:21:09 +0300

    yet another

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from enum import Enum
 from fastapi import FastAPI, HTTPException
 from fastapi import param_functions
 from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -37,30 +38,61 @@ post_db = [
     Timestamp(id=1, timestamp=10)
 ]
 
-
+# возвращает строку
 @app.get('/', summary='Root')
 def root():
     return {'Hello'}
 
-@app.post('/post')
+# добавляем объект в post_db, который на +1 отличается от предыдущего объекта
+@app.post('/post',response_model=Timestamp, summary='Get Post')
 def post():
-    pass
+    post_db.append(Timestamp(id=post_db[-1].id + 1, timestamp=post_db[-1].timestamp + 1))
+    return {
+        'id': post_db[-1].id,
+        'timestamp': post_db[-1].timestamp
+    }
 
-@app.get('/dog', summary='Get Dogs')
-def get_dog(kind: DogType):
-    return dogs_db[DogType.kind]
+# вытасикваем собакенов по породе. Если порода не указана - вытаскиваем всех собакенов
+@app.get('/dog', response_model=List[Dog], summary='Get Dogs')
+def get_dog(kind: DogType = None):
+    if kind is None:
+        return dogs_db.values()
+    else:
+        return [i for i in dogs_db.values() if i.kind == kind]
 
-@app.get('/dog/{pk}', summary='Get Dog By Pk')
-def get_dogs_by_pk(pk: int):
-    return dogs_db[pk]
-
+# добавляем собакена в базу данных. Если собака с таким pk уже существует - выкидываем 409
 @app.post('/dog', response_model=Dog, summary='Create Dog')
 def create_dog(dog: Dog):
-    existing_pk = ...
-    if existing_pk:
+    if dog.pk in [i.pk for i in dogs_db.values()]:
         raise HTTPException(status_code=409,
-                            detail='The specified PK already exists.')
-    
-    ...
+                            detail='The specified PK already exists')
+    else:
+        dogs_db.update({list(dogs_db.keys())[-1] + 1: dog})
+        return dog
 
-    return Dog(...)
+# ищем собакена по pk. Если записи с таким PK не существует - возвращаем 409
+@app.get('/dog/{pk}', response_model=Dog, summary='Get Dog By Pk')
+def get_dog_by_pk(pk: int):
+    if len([i for i in dogs_db.values() if i.pk == pk]) == 0:
+        raise HTTPException(status_code=409,
+                            detail='No such PK in database')
+    else:
+        return [i for i in dogs_db.values() if i.pk == pk][0]
+    
+# апдетйим собакена по PK. Если такого РК не существует - даем 409 
+@app.patch('/dog/{pk}', response_model=Dog, summary='Update Dog')
+def create_dog(pk: int, dog: Dog):
+    if len([i for i in dogs_db.values() if i.pk == pk]) == 0:
+        raise HTTPException(status_code=409,
+                            detail='No such PK in database')
+    else:
+        k = [k for k, v in dogs_db.items() if v.pk == pk] # список записей с таким pk (по идее должна быть одна, но на всякий удалим все)
+
+        #удаление записей 
+        for i in k:
+            del dogs_db[i]
+
+        #добавление записи
+        dogs_db.update({list(dogs_db.keys())[-1] + 1: dog})
+        return dog
+    
